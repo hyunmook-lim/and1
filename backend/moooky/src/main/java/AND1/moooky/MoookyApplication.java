a64e6e900e2c2ec23d1449fae72a89b08dbf6baa
hyunmook-lim <hmlim0320@naver.com>
2022-08-09 19:47:30 +0900

    spring starter

---
+++ backend/moooky/src/main/java/AND1/moooky/MoookyApplication.java
@@ -0,0 +1,13 @@
+package AND1.moooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoookyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoookyApplication.class, args);
+	}
+
+}
---
+++ backend/moooky/src/test/java/AND1/moooky/MoookyApplicationTests.java
@@ -0,0 +1,13 @@
+package AND1.moooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoookyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
